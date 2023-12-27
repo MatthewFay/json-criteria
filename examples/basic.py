04f29f017d11ec817916f5300a94f8f60ef7ed86
MatthewFay <mefay@hotmail.com>
2023-12-26 19:11:01 -0800

    add basic example

--- examples/basic.py
+++ examples/basic.py
@@ -1,1 +1,5 @@
-# TODO
+from json_criteria import satisfies_criteria
+
+record = { 'name': 'Joe' , 'age': 30 }
+criteria = { 'key': 'age', 'op': 'equals', 'value': 30 }
+result = satisfies_criteria(record, criteria)
