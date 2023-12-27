fc34e595bc16356f85e4b0348e1a4a8c5da38599
MatthewFay <mefay@hotmail.com>
2023-12-26 19:48:20 -0800

    add more operators

--- src/json_criteria/satisfies_criteria.py
+++ src/json_criteria/satisfies_criteria.py
@@ -19,7 +19,22 @@ def satisfies_criteria(record: Dict[str, Any], element: Dict[str, Any]) -> bool:
     if not isinstance(key, str):
         raise ValueError(f'Invalid key: {key}')
 
-    if operator == 'equals':
+    if value is None:
+        raise ValueError(f'Invalid value: {value}')
+
+    if operator == 'equal_to':
         return record.get(key) == value
+    if operator == 'not_equal_to':
+        return record.get(key) != value
+    if operator == 'greater_than':
+        return record.get(key) > value
+    if operator == 'less_than':
+        return record.get(key) < value
+    if operator == 'greater_than_or_equal_to':
+        return record.get(key) >= value
+    if operator == 'less_than_or_equal_to':
+        return record.get(key) <= value
+    if operator == 'in':
+        return value in record.get(key)
 
     raise ValueError(f'Invalid operator: {operator}')
--- tests/test_satisfies_criteria.py
+++ tests/test_satisfies_criteria.py
@@ -5,7 +5,17 @@ from src.json_criteria import satisfies_criteria
 class TestSatisfiesCriteria(unittest.TestCase):
     def test_basic(self):
         record = { 'name': 'Joe' , 'age': 30 }
-        criteria = { 'key': 'age', 'op': 'equals', 'value': 30 }
+        criteria = { 'key': 'age', 'op': 'equal_to', 'value': 30 }
+        result = satisfies_criteria(record, criteria)
+        self.assertTrue(result)
+
+    def test_basic2(self):
+        record = { 'name': 'Joe' , 'age': 30 }
+        criteria = { 'AND': [
+            {'key': 'age', 'op': 'equal_to', 'value': 30 },
+            {'key': 'name', 'op': 'equal_to', 'value': 'Joe' }
+            ]
+        }
         result = satisfies_criteria(record, criteria)
         self.assertTrue(result)
 
