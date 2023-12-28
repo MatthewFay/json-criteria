913c21366375bbbc0d47a815c05da7656a00d679
MatthewFay <mefay@hotmail.com>
2023-12-27 20:21:01 -0800

    add internal modules, add tests

--- src/json_criteria/satisfies_criteria.py
+++ src/json_criteria/satisfies_criteria.py
@@ -1,40 +1,5 @@
 from typing import Dict, Any
+from .internal import satisfies_crit
 
-def satisfies_criteria(record: Dict[str, Any], element: Dict[str, Any]) -> bool:
-    # TODO: `element` validation
-
-    # AND, OR, NOT operators
-    if 'AND' in element:
-        return all(satisfies_criteria(record, el) for el in element.get('AND', []))
-    if 'OR' in element:
-        return any(satisfies_criteria(record, el) for el in element.get('OR', []))
-    if 'NOT' in element:
-        return not satisfies_criteria(record, element.get('NOT', {}))
-
-    # Conditions
-    key = element.get('key')
-    operator = element.get('op')
-    value = element.get('value')
-
-    if not isinstance(key, str):
-        raise ValueError(f'Invalid key: {key}')
-
-    if value is None:
-        raise ValueError(f'Invalid value: {value}')
-
-    if operator == 'equal_to':
-        return record.get(key) == value
-    if operator == 'not_equal_to':
-        return record.get(key) != value
-    if operator == 'greater_than':
-        return record.get(key) > value
-    if operator == 'less_than':
-        return record.get(key) < value
-    if operator == 'greater_than_or_equal_to':
-        return record.get(key) >= value
-    if operator == 'less_than_or_equal_to':
-        return record.get(key) <= value
-    if operator == 'in':
-        return value in record.get(key)
-
-    raise ValueError(f'Invalid operator: {operator}')
+def satisfies_criteria(record: Dict[str, Any], criteria: Dict[str, Any]) -> bool:
+    return satisfies_crit(record, criteria)
--- tests/test_satisfies_criteria.py
+++ tests/test_satisfies_criteria.py
@@ -19,5 +19,15 @@ class TestSatisfiesCriteria(unittest.TestCase):
         result = satisfies_criteria(record, criteria)
         self.assertTrue(result)
 
+    def test_basic3(self):
+        record = { 'name': 'Joe' , 'age': 30 }
+        criteria = { 'AND': [
+            {'key': 'age', 'op': 'equal_to', 'value': 30 },
+            {'key': 'name', 'op': 'equal_to', 'value': 'Steve' }
+            ]
+        }
+        result = satisfies_criteria(record, criteria)
+        self.assertFalse(result)
+
 if __name__ == '__main__':
     unittest.main()
