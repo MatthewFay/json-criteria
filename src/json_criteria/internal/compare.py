1f207bd472362768c39aa8eff2e72c7e66570f01
MatthewFay <mefay@hotmail.com>
2024-01-03 19:41:56 -0800

    improve internals

---
+++ src/json_criteria/internal/compare.py
@@ -0,0 +1,138 @@
+from typing import Any
+import re
+
+def compare_equal_to(record_value: Any, value: Any) -> bool:
+    """Check if the record value is equal to the specified value."""
+    return record_value == value
+
+def compare_not_equal_to(record_value: Any, value: Any) -> bool:
+    """Check if the record value is not equal to the specified value."""
+    return record_value != value
+
+def compare_greater_than(record_value: Any, value: Any) -> bool:
+    """Check if the record value is greater than the specified value."""
+    return record_value > value
+
+def compare_less_than(record_value: Any, value: Any) -> bool:
+    """Check if the record value is less than the specified value."""
+    return record_value < value
+
+def compare_greater_than_or_equal_to(record_value: Any, value: Any) -> bool:
+    """Check if the record value is greater than or equal to the specified value."""
+    return record_value >= value
+
+def compare_less_than_or_equal_to(record_value: Any, value: Any) -> bool:
+    """Check if the record value is less than or equal to the specified value."""
+    return record_value <= value
+
+def compare_in(record_value: Any, value: Any) -> bool:
+    """Check if the record value is in the specified value."""
+    return record_value in value
+
+def compare_not_in(record_value: Any, value: Any) -> bool:
+    """Check if the record value is not in the specified value."""
+    return record_value not in value
+
+def compare_contains(record_value: Any, value: Any) -> bool:
+    """Check if the record value contains the specified value."""
+    return value in record_value
+
+def compare_not_contains(record_value: Any, value: Any) -> bool:
+    """Check if the record value does not contain the specified value."""
+    return value not in record_value
+
+def compare_starts_with(record_value: str, value: Any) -> bool:
+    """Check if the record value starts with the specified value."""
+    return record_value.startswith(value)
+
+def compare_not_starts_with(record_value: str, value: Any) -> bool:
+    """Check if the record value does not start with the specified value."""
+    return not record_value.startswith(value)
+
+def compare_ends_with(record_value: str, value: Any) -> bool:
+    """Check if the record value ends with the specified value."""
+    return record_value.endswith(value)
+
+def compare_not_ends_with(record_value: str, value: Any) -> bool:
+    """Check if the record value does not end with the specified value."""
+    return not record_value.endswith(value)
+
+def compare_matches_regex(record_value: Any, value: Any) -> bool:
+    """Check if the record value matches the specified regex value."""
+    return re.search(value, str(record_value)) is not None
+
+operator_functions = {
+    'equal_to': compare_equal_to,
+    'not_equal_to': compare_not_equal_to,
+    'greater_than': compare_greater_than,
+    'less_than': compare_less_than,
+    'greater_than_or_equal_to': compare_greater_than_or_equal_to,
+    'less_than_or_equal_to': compare_less_than_or_equal_to,
+    'in': compare_in,
+    'not_in': compare_not_in,
+    'contains': compare_contains,
+    'not_contains': compare_not_contains,
+    'starts_with': compare_starts_with,
+    'not_starts_with': compare_not_starts_with,
+    'ends_with': compare_ends_with,
+    'not_ends_with': compare_not_ends_with
+}
+
+def compare(record_value: Any, operator: str, value: Any) -> bool:
+    """
+    Compare record value with the specified value using the given operator.
+
+    Parameters:
+    - record_value (Any): The value from the data record.
+    - operator (str): The comparison operator.
+    - value (Any): The value to compare against.
+
+    Returns:
+    - bool: The result of the comparison.
+    """
+
+    if operator in operator_functions:
+        return operator_functions[operator](record_value, value)
+
+    raise ValueError(f"Unsupported operator: {operator}")
+
+# if operator == 'is_empty':
+    #     return not bool(record.get(key))
+    # if operator == 'is_not_empty':
+    #     return bool(record.get(key))
+    # if operator == 'is_true':
+    #     return bool(record.get(key))
+    # if operator == 'is_false':
+    #     return not bool(record.get(key))
+    # if operator == 'is_null':
+    #     return record.get(key) is None
+    # if operator == 'is_not_null':
+    #     return record.get(key) is not None
+    # if operator == 'equals_ignore_case':
+    #     return record.get(key, '').lower() == value.lower()
+    # if operator == 'not_equals_ignore_case':
+    #     return record.get(key, '').lower() != value.lower()
+    # if operator == 'is_date':
+    #     try:
+    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
+    #         return True
+    #     except ValueError:
+    #         return False
+    # if operator == 'date_equal_to':
+    #     try:
+    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
+    #         return parsed_date == value
+    #     except ValueError:
+    #         return False
+    # if operator == 'date_before':
+    #     try:
+    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
+    #         return parsed_date < value
+    #     except ValueError:
+    #         return False
+    # if operator == 'date_after':
+    #     try:
+    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
+    #         return parsed_date > value
+    #     except ValueError:
+    #         return False
--- src/json_criteria/internal/meets_crit.py
+++ src/json_criteria/internal/meets_crit.py
@@ -1,6 +1,5 @@
 from typing import Dict, Any
-# from datetime import datetime
-import re
+from .compare import compare
 
 def meets_crit(record: Dict[str, Any], element: Dict[str, Any]) -> bool:
     """
@@ -33,78 +32,4 @@ def meets_crit(record: Dict[str, Any], element: Dict[str, Any]) -> bool:
     if not isinstance(key, str) or not operator or value is None:
         raise ValueError(f'Invalid condition: {element}')
 
-    # Operator conditions - WIP
-    # Change to dictionary where key is operator, value is compare function,
-    # and break out into another module.
-    if operator == 'equal_to':
-        return record.get(key) == value
-    if operator == 'not_equal_to':
-        return record.get(key) != value
-    if operator == 'greater_than':
-        return record.get(key) > value
-    if operator == 'less_than':
-        return record.get(key) < value
-    if operator == 'greater_than_or_equal_to':
-        return record.get(key) >= value
-    if operator == 'less_than_or_equal_to':
-        return record.get(key) <= value
-    if operator == 'in':
-        return record.get(key) in value
-    if operator == 'not_in':
-        return record.get(key) not in value
-    if operator == 'contains':
-        return value in record.get(key)
-    if operator == 'not_contains':
-        return value not in record.get(key)
-    if operator == 'starts_with':
-        return record.get(key, '').startswith(value)
-    if operator == 'not_starts_with':
-        return not record.get(key, '').startswith(value)
-    if operator == 'ends_with':
-        return record.get(key, '').endswith(value)
-    if operator == 'not_ends_with':
-        return not record.get(key, '').endswith(value)
-    # if operator == 'is_empty':
-    #     return not bool(record.get(key))
-    # if operator == 'is_not_empty':
-    #     return bool(record.get(key))
-    # if operator == 'is_true':
-    #     return bool(record.get(key))
-    # if operator == 'is_false':
-    #     return not bool(record.get(key))
-    # if operator == 'is_null':
-    #     return record.get(key) is None
-    # if operator == 'is_not_null':
-    #     return record.get(key) is not None
-    if operator == 'matches_regex':
-        return re.search(value, str(record.get(key))) is not None
-    # if operator == 'equals_ignore_case':
-    #     return record.get(key, '').lower() == value.lower()
-    # if operator == 'not_equals_ignore_case':
-    #     return record.get(key, '').lower() != value.lower()
-    # if operator == 'is_date':
-    #     try:
-    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
-    #         return True
-    #     except ValueError:
-    #         return False
-    # if operator == 'date_equal_to':
-    #     try:
-    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
-    #         return parsed_date == value
-    #     except ValueError:
-    #         return False
-    # if operator == 'date_before':
-    #     try:
-    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
-    #         return parsed_date < value
-    #     except ValueError:
-    #         return False
-    # if operator == 'date_after':
-    #     try:
-    #         parsed_date = datetime.strptime(record.get(key, ''), '%Y-%m-%d')
-    #         return parsed_date > value
-    #     except ValueError:
-    #         return False
-
-    raise ValueError(f'Invalid operator: {operator}')
+    return compare(record.get(key), operator, value)
